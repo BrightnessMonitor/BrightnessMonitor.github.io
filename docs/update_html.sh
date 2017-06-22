99fb0ce3d003f66e116b3f036428b70ade34163d
Steffen Exler <steffen.exler@gmail.com>
2017-06-22 23:59:12 +0200

    update theme & update script

--- docs/update_html.sh
+++ docs/update_html.sh
@@ -6,13 +6,9 @@ make html
 # rm the old html files
 rm -r ../_sources
 rm -r ../_static
-rm ../.buildinfo
-rm ../.nojekyll
-rm ../genindex.html
-rm ../index.html
+rm ../*.html
 rm ../objects.inv
-rm ../search.html
-rm ../searchindex.js
+rm ../*.js
 
 # copy the html files to the root folder
 cp -r build/html/* ../
--- searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{modulindex:0,stichwortverzeichnis:0,such:0},titles:["Welcome to Brightness Monitor Doc\u2019s documentation!"],titleterms:{and:0,brightness:0,doc:0,documentation:0,indic:0,monitor:0,tabl:0,welcom:0}})
